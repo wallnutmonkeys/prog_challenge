fc1e26a40f2222ceebf1ef498b1cf436e5aa69ad
dominic <dpaquette@ubity.com>
2017-09-04 12:15:58 -0400

    add multi-thread support (1 thread per client)

--- aor_lookup.py
+++ aor_lookup.py
@@ -5,10 +5,43 @@ import sys
 import json
 from registers import Registers
 import logging
+import thread
 
 REGISTRATION_FILEPATH = './registrations.json'
 PORT = 5000
 CONNECTION_TIMEOUT = 10.0
+
+
+
+def process_client_thread(connection, client_address, registers):
+
+    try:
+        rcv_buff = ''
+        while True:
+            data = connection.recv(1)
+            if data != '\n':
+                rcv_buff += data
+            else:
+                aor = rcv_buff
+                logging.info("received a lookup request for aor: %s from client %s" % (aor, str(client_address)))
+                ret = ''
+                try:
+                    ret = registers.lookup(aor)
+                    logging.info("found matching register(s) for aor: %s" % aor)
+                except KeyError:
+                    logging.info("no match found for aor: %s" % aor)
+
+                logging.info("sending back: %s to %s" % (ret,str(client_address)))
+                connection.sendall(ret + "\n")
+
+                rcv_buff = ''
+
+    except socket.timeout:
+        logging.warning("connection to %s has timed out" % str(client_address))
+
+    finally:
+        logging.info("closing connection to %s" % str(client_address))
+        connection.close()
 
 
 if __name__ == "__main__":
@@ -26,39 +59,13 @@ if __name__ == "__main__":
     logging.info("listening on port %d" % PORT)
 
     while True:
-        print "waiting for a connection"
         connection, client_address = sock.accept()
+        logging.info("accepted connection from %s" % str(client_address))
+
         connection.settimeout(CONNECTION_TIMEOUT)
-        try:
-            logging.info("accepting connection from %s" % str(client_address))
-       
-            rcv_buff = ''
+        logging.info("starting new thread to process request from %s" % str(client_address))
 
-            while True:
-                data = connection.recv(1)
-                if data != '\n':
-                    rcv_buff += data
-                else:
-                    aor = rcv_buff
-                    logging.info("received a lookup request for aor: %s from client %s" % (aor, str(client_address)))
-                    ret = ''
-                    try:
-                        ret = registers.lookup(aor)
-                        logging.info("found matching register(s) for aor: %s" % aor)
-                    except KeyError:
-                        logging.info("no match found for aor: %s" % aor)
-
-                    logging.info("sending back: %s to %s" % (ret,str(client_address)))
-                    connection.sendall(ret + "\n")
-
-                    rcv_buff = ''
-
-        except socket.timeout:
-            logging.warning("connection to %s has timed out" % str(client_address))
-
-        finally:
-            logging.info("closing connection to %s" % str(client_address))
-            connection.close()
+        thread.start_new_thread(process_client_thread, (connection, client_address, registers))
 
 
 
