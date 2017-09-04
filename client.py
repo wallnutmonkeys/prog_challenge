9db8263cf1b59530d26181bf5dcb2578f128cae2
dominic <dpaquette@ubity.com>
2017-09-03 22:53:46 -0400

    add 2 valid aor lookups followed by 1 bogus aor lookup and a connection timeout

--- client.py
+++ client.py
@@ -22,22 +22,22 @@ if __name__ == "__main__":
     sock = socket.socket()
     sock.connect(('localhost', 5000))
 
-    sock.sendall("myaor1\n")
+    sock.sendall("01577bc1415ed95760000100620002\n")
     srv_response = receive_response(sock)
     print "received %s back from server" % srv_response
     time.sleep(3)
 
-    sock.sendall("myaor2\n")
+    sock.sendall("0148c1f489badb837d000100620002\n")
     srv_response = receive_response(sock)
     print "received %s back from server" % srv_response
     time.sleep(5)
 
-    sock.sendall("myaor3\n")
+    sock.sendall("01576e593243534655555555620007\n")
     srv_response = receive_response(sock)
     print "received %s back from server" % srv_response
     time.sleep(15)
 
-    sock.sendall("myaor4\n")
+    sock.sendall("34534503948503498503495\n")
     srv_response = receive_response(sock)
     print "received %s back from server" % srv_response
 
