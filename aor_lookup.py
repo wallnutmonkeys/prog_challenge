02a1d6e990f572d7619b118b6624c2f889af3fb4
dominic <dpaquette@ubity.com>
2017-09-03 22:26:16 -0400

    add loading of the registers into a list

--- aor_lookup.py
+++ aor_lookup.py
@@ -2,38 +2,50 @@
 
 import socket
 import sys
+import json
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 5000)
+def load_json(json_filepath):
+    registers = []
+    with open(json_filepath,'r') as reg_file:
+        for line in reg_file:
+            registers.append(json.loads(line))
+    return registers
 
-sock.bind(server_address)
+if __name__ == "__main__":
 
-sock.listen(1)
+    registers = load_json('./registrations.json')
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 5000)
 
-while True:
-    print "waiting for a connection"
-    connection, client_address = sock.accept()
-    connection.settimeout(10.0)
-    try:
-        print "connection from", client_address
+    sock.bind(server_address)
+
+    sock.listen(1)
+
+    while True:
+        print "waiting for a connection"
+        connection, client_address = sock.accept()
+        connection.settimeout(10.0)
+        try:
+            print "connection from", client_address
        
-        rcv_buff = ''
+            rcv_buff = ''
 
-        while True:
-            data = connection.recv(1)
-            if data != '\n':
-                rcv_buff += data
-            else:
-                print "received: %s" % (rcv_buff,)
-                response = "blablablablabla"
-                print "sending back %s" % response
-                connection.sendall(response + "\n")
-                rcv_buff = ''
-    except socket.timeout:
-        print "timeout"
+            while True:
+                data = connection.recv(1)
+                if data != '\n':
+                    rcv_buff += data
+                else:
+                    print "received: %s" % (rcv_buff,)
+                    response = "blablablablabla"
+                    print "sending back %s" % response
+                    connection.sendall(response + "\n")
+                    rcv_buff = ''
+        except socket.timeout:
+            print "timeout"
 
-    finally:
-        connection.close()
+        finally:
+            connection.close()
 
 
 
