c100099521abff6855125c9d9a7c2ebab895615b
dominic <dpaquette@ubity.com>
2017-09-04 09:27:43 -0400

    add logging and replace hard coded values with constants

--- aor_lookup.py
+++ aor_lookup.py
@@ -4,24 +4,33 @@ import socket
 import sys
 import json
 from registers import Registers
+import logging
+
+REGISTRATION_FILEPATH = './registrations.json'
+PORT = 5000
+CONNECTION_TIMEOUT = 10.0
 
 
 if __name__ == "__main__":
 
+    logging.basicConfig(filename='aor_lookup.log',level=logging.DEBUG,format='%(asctime)s %(levelname)s %(message)s')
+    logging.info("starting up the server")
+
     registers = Registers()
-    registers.from_file('registrations.json')
+    registers.from_file(REGISTRATION_FILEPATH)
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 5000)
+    server_address = ('localhost', PORT)
     sock.bind(server_address)
     sock.listen(1)
+    logging.info("listening on port %d" % PORT)
 
     while True:
         print "waiting for a connection"
         connection, client_address = sock.accept()
-        connection.settimeout(10.0)
+        connection.settimeout(CONNECTION_TIMEOUT)
         try:
-            print "connection from", client_address
+            logging.info("accepting connection from %s" % str(client_address))
        
             rcv_buff = ''
 
@@ -31,20 +40,24 @@ if __name__ == "__main__":
                     rcv_buff += data
                 else:
                     aor = rcv_buff
-                    print "looking up aor: %s" % aor
+                    logging.info("received a lookup request for aor: %s from client %s" % (aor, str(client_address)))
                     ret = ''
                     try:
                         ret = registers.lookup(aor)
+                        logging.info("found matching register(s) for aor: %s" % aor)
                     except KeyError:
-                        print "register with aor:'%s' not found" % aor
+                        logging.info("no match found for aor: %s" % aor)
 
+                    logging.info("sending back: %s to %s" % (ret,str(client_address)))
                     connection.sendall(ret + "\n")
 
                     rcv_buff = ''
+
         except socket.timeout:
-            print "timeout"
+            logging.warning("connection to %s has timed out" % str(client_address))
 
         finally:
+            logging.info("closing connection to %s" % str(client_address))
             connection.close()
 
 
