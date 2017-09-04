67ce678a7e8956742ec923ebf494109918e90eb0
dominic <dpaquette@ubity.com>
2017-09-04 08:36:53 -0400

    convert to use Registers class for lookups

--- aor_lookup.py
+++ aor_lookup.py
@@ -10,15 +10,10 @@ if __name__ == "__main__":
 
     registers = Registers()
     registers.from_file('registrations.json')
-    print registers
-    exit(1)
-    
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ('localhost', 5000)
-
     sock.bind(server_address)
-
     sock.listen(1)
 
     while True:
@@ -37,12 +32,14 @@ if __name__ == "__main__":
                 else:
                     aor = rcv_buff
                     print "looking up aor: %s" % aor
-                    register = lookup_aor(registers, aor)
-                    if not register:
+                    ret = ''
+                    try:
+                        ret = registers.lookup(aor)
+                    except KeyError:
                         print "register with aor:'%s' not found" % aor
-                    else:
-                        print "register with aor:'%s' was found" % aor
-                        connection.sendall(json.dumps(register) + "\n")
+
+                    connection.sendall(ret + "\n")
+
                     rcv_buff = ''
         except socket.timeout:
             print "timeout"
