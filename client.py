7a7061f8eec8c99051499bbd59a25c14172ae1d2
dominic <dpaquette@ubity.com>
2017-09-03 20:34:55 -0400

    add sleeps to test the 10 second connection timeout

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 
 import socket
 import sys
+import time
 
 
 def receive_response(sock):
@@ -24,14 +25,17 @@ if __name__ == "__main__":
     sock.sendall("myaor1\n")
     srv_response = receive_response(sock)
     print "received %s back from server" % srv_response
+    time.sleep(3)
 
     sock.sendall("myaor2\n")
     srv_response = receive_response(sock)
     print "received %s back from server" % srv_response
+    time.sleep(5)
 
     sock.sendall("myaor3\n")
     srv_response = receive_response(sock)
     print "received %s back from server" % srv_response
+    time.sleep(15)
 
     sock.sendall("myaor4\n")
     srv_response = receive_response(sock)
